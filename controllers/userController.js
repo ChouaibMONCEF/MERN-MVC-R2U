6b06b237de096e6537ce6414203124269b5dfe95
choaib moncef <choaibmoncef1@gmail.com>
2022-01-11 16:44:47 +0100

    Few Updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,12 @@
+const User = require("../models/userModel")
+
 module.exports = {
+
     get : (req, res) => {
         res.send("new BullShit!!")
+    },
+    blabla : (req, res) => {
+        res.send("new BullShit!!")
     }
+
 }
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const session = require("express-session")
 require('./env')
+const mongoose = require('mongoose')
 const app = express()
 
+mongoose.connect(database)
 
 const userRoute = require("./routes/User")
 
